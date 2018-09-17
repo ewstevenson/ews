41b4009063b0841c8448608c83a43e2d029e5db2
Eric Stevenson <ews@erics-mbp.corp.adobe.com>
2018-09-17 16:10:03 -0600

    clean up cruft

--- browser-extentions/chrome/c2c-dl/js/content.js
+++ browser-extentions/chrome/c2c-dl/js/content.js
@@ -1,32 +1,13 @@
-
-// functions
+// there is no sleep, so make some
 function sleep (time) {
   return new Promise((resolve) => setTimeout(resolve, time));
 }
 
-
-var HttpClient = function() {
-    this.get = function(aUrl, aCallback) {
-        var anHttpRequest = new XMLHttpRequest();
-        anHttpRequest.onreadystatechange = function() {
-            if (anHttpRequest.readyState == 4 && anHttpRequest.status == 200)
-                aCallback(anHttpRequest.responseText);
-        }
-
-        anHttpRequest.open( "GET", aUrl, true );
-        anHttpRequest.send( null );
-    }
-}
-
-
-
 // sleep until all DOM elements are populated.. else youll get # instead of URLs
 sleep(500).then(() => {
-
   var filter = {urls: ["<all_urls>"]};
-  //var arr = [], l = document.getElementsByClassName("audiobox4");
+  // useful links are of the streamlink-dynamic class
   var streamLinks = document.getElementsByClassName("streamlink-dynamic");
-  //console.log(streamLinks);
   for(var i=0; i<streamLinks.length; i++) {
     if (streamLinks.item(i).className == 'streamlink-dynamic') {
       //console.log(streamLinks[i].attributes[0]);
@@ -34,39 +15,9 @@ sleep(500).then(() => {
       //console.log(streamLinks[i].attributes[0].ownerElement.href);
       //console.log(streamLinks[i].attributes[0].value);
       //console.log(streamLinks[i].attributes[0].textContent);
-        chrome.runtime.sendMessage(streamLinks[i].href);
-
-
-    //  chrome.downloads.download({
-      //    url: streamLinks[i].href,
-        //  saveAs: false
-
-    //  });
-
+      chrome.runtime.sendMessage(streamLinks[i].href);
       //console.log(streamLinks[i].attributes[0].nodeValue);
       //console.log(streamLinks.item(i).className );
+    }
   }
-
-}
 })
-
-/*
-var v = document.getElementsByClassName("streamlink-dynamic");
-console.log(v);
-
-var items = new Array();
-for (var j = 0; j < v.length; j++){
-  console.log(v.item+" is the link.");
-  items[j] = v.item(j);
-}
-
-var client = new HttpClient();
-client.get('https://ews.consulting/c2c.php?s='+items, function(response) {
-    // do something with response
-    console.log(response);
-  });
-
-
-console.log(links);
-console.log(anchors);
-*/
