45923984527f911bbb334b659be252847601551f
Eric Stevenson <ews@erics-mbp.corp.adobe.com>
2018-09-17 16:25:45 -0600

    create date arrays

--- browser-extentions/chrome/c2c-dl/js/content.js
+++ browser-extentions/chrome/c2c-dl/js/content.js
@@ -1,11 +1,22 @@
 // there is no sleep, so make some
+
+
+ var baseURL = 'https://www.coasttocoastam.com/show/';
+ var years = ['2013', '2014', '2015', '2016', '2017', '2018'];
+ var months = ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12'];
+ var days = ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10',
+              '11', '12', '13', '14', '15', '16', '17', '18', '19', '20',
+              '21', '22', '23', '24', '25', '26', '27', '28', '29', '30', '31'];
+
+              
+
 function sleep (time) {
   return new Promise((resolve) => setTimeout(resolve, time));
 }
 
 // sleep until all DOM elements are populated.. else youll get # instead of URLs
 sleep(500).then(() => {
-  var filter = {urls: ["<all_urls>"]};
+  var filter = {urls: ["https://www.coasttocoastam.com/*"]};
   // useful links are of the streamlink-dynamic class
   var streamLinks = document.getElementsByClassName("streamlink-dynamic");
   for(var i=0; i<streamLinks.length; i++) {
