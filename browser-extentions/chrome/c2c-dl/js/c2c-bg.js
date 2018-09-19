5574ea622773a6de3f6e55d468284dc437fa60ff
ewstevenson <ewsutllc@gmail.com>
2018-09-19 06:38:49 -0600

    ha!

--- browser-extentions/chrome/c2c-dl/js/c2c-bg.js
+++ browser-extentions/chrome/c2c-dl/js/c2c-bg.js
@@ -4,51 +4,12 @@
 // orignal code
 
 chrome.runtime.onMessage.addListener(function(mp3_url, sender, sendResponse) {
-  // huge hack
-  var baseURL = 'https://www.coasttocoastam.com/show/';
-  var years = ['2013', '2014', '2015', '2016', '2017', '2018'];
-  var months = [ '01', '02', '03', '04', '05', '06', '07', '08', '09', '10',
-                 '11', '12'];
-  var days = [ '01', '02', '03', '04', '05', '06', '07', '08', '09', '10',
-               '11', '12', '13', '14', '15', '16', '17', '18', '19', '20',
-               '21', '22', '23', '24', '25', '26', '27', '28', '29', '30',
-               '31'];
 
-  var day;
-  var month;
-  var year;
-  var datePath;
-  var showURL;
-
-  for (var y = 0; y < years.length; y++) {
-    year = years[y];
-    for (var m = 0; m < months.length; m++) {
-      month = months[m];
-      for (var d = 0; d < days.length; d++){
-        day = days[d];
-        datePath = year+"/"+month+"/"+day;
-        showURL = baseURL+datePath;
-        // do this for everyday in loop
-        console.log(showURL);
-      }
-    }
-  }
-
-  // end huge hack
-
-
-
-
-console.log(mp3_url);
-//chrome.tabs.create({ url: mp3_url });
-
-
-/*
-		chrome.downloads.download({
-			url: url,
-			saveAs: false
-		});
-		*/
+	console.log(mp3_url);
+	chrome.downloads.download({
+		url: mp3_url,
+		saveAs: false
+	});
 });
 
 //end original code
--- browser-extentions/chrome/c2c-dl/js/content.js
+++ browser-extentions/chrome/c2c-dl/js/content.js
@@ -1,34 +1,32 @@
 
 //var filter = {urls: <all_urls>};
-chrome.runtime.sendMessage("https://www.coasttocoastam.com/show/2018/09/01");
+//chrome.runtime.sendMessage("https://www.coasttocoastam.com/show/2018/09/01");
 
 
 // there is no sleep, so make some
-/*
+
 
 function sleep (time) {
   return new Promise((resolve) => setTimeout(resolve, time));
 }
 
- var baseURL = 'https://www.coasttocoastam.com/show/';
- var years = ['2013', '2014', '2015', '2016', '2017', '2018'];
- var months = [ '01', '02', '03', '04', '05', '06', '07', '08', '09', '10',
-                '11', '12'];
- var days = [ '01', '02', '03', '04', '05', '06', '07', '08', '09', '10',
-              '11', '12', '13', '14', '15', '16', '17', '18', '19', '20',
-              '21', '22', '23', '24', '25', '26', '27', '28', '29', '30',
-              '31'];
+// huge hack
+var baseURL = 'https://www.coasttocoastam.com/show/';
+var years = ['2013', '2014', '2015', '2016', '2017', '2018'];
+var months = [ '01', '02', '03', '04', '05', '06', '07', '08', '09', '10',
+               '11', '12'];
+var days = [ '01', '02', '03', '04', '05', '06', '07', '08', '09', '10',
+             '11', '12', '13', '14', '15', '16', '17', '18', '19', '20',
+             '21', '22', '23', '24', '25', '26', '27', '28', '29', '30',
+             '31'];
 
 var day;
 var month;
 var year;
 var datePath;
 var showURL;
+var all_dates = [];
 
-*/
-
-
-/*
 for (var y = 0; y < years.length; y++) {
   year = years[y];
   for (var m = 0; m < months.length; m++) {
@@ -37,44 +35,37 @@ for (var y = 0; y < years.length; y++) {
       day = days[d];
       datePath = year+"/"+month+"/"+day;
       showURL = baseURL+datePath;
+      all_dates.push(showURL);
       // do this for everyday in loop
-      //console.log(showURL);
+      // console.log(showURL);
+    }
+  }
+}
 
-*/
+console.log("Current Location: "+window.location.href);
+console.log("Next Location: "+all_dates[all_dates.indexOf(window.location.href)+1]);
+// end huge hack
+
 
       // sleep until all DOM elements are populated.. else youll get # instead of URLs
-/*
 
-      sleep(500).then(() => {
-        var filter = {urls: ["https://www.coasttocoastam.com/*"]};
-        // useful links are of the streamlink-dynamic class
-        var streamLinks = document.getElementsByClassName("streamlink-dynamic");
-        for(var i=0; i<streamLinks.length; i++) {
-          if (streamLinks.item(i).className == 'streamlink-dynamic') {
-            console.log(streamLinks[i].attributes[0]);
-            //console.log(streamLinks[i].parentNode.firstElementChild.href);
-            //console.log(streamLinks[i].attributes[0].ownerElement.href);
-            //console.log(streamLinks[i].attributes[0].value);
-            //console.log(streamLinks[i].attributes[0].textContent);
+sleep(500).then(() => {
+  //  var filter = {urls: ["https://www.coasttocoastam.com/*"]};
+  // useful links are of the streamlink-dynamic class
+  var streamLinks = document.getElementsByClassName("streamlink-dynamic");
+  for(var i=0; i<streamLinks.length; i++) {
+    if (streamLinks.item(i).className == 'streamlink-dynamic') {
+      //console.log(streamLinks[i].attributes[0]);
+      //console.log(streamLinks[i].parentNode.firstElementChild.href);
+      //console.log(streamLinks[i].attributes[0].ownerElement.href);
+      //console.log(streamLinks[i].attributes[0].value);
+      //console.log(streamLinks[i].attributes[0].textContent);
+      // use this one to get mp3 url
+      chrome.runtime.sendMessage(streamLinks[i].href);
+      //console.log(streamLinks[i].attributes[0].nodeValue);
+      //console.log(streamLinks.item(i).className );
+    }
+  }
+})
 
-            // use this one to get mp3 url
-            chrome.runtime.sendMessage(streamLinks[i].href);
-
-            //console.log(streamLinks[i].attributes[0].nodeValue);
-            //console.log(streamLinks.item(i).className );
-          }
-        }
-      })
-*/
-
-
-
-
-/*
-                  } // end of do this everyday loop
-
-
-                }
-
-              }
-              */
+  window.location.href = all_dates[all_dates.indexOf(window.location.href)+1];
