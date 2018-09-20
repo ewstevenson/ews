491d1ca5b87e0ef5a814f3e0f7293df0184e5dec
ewstevenson <ewsutllc@gmail.com>
2018-09-20 17:47:42 -0600

    clean up

--- browser-extentions/chrome/c2c-dl/js/c2c-bg.js
+++ browser-extentions/chrome/c2c-dl/js/c2c-bg.js
@@ -5,11 +5,13 @@
 
 chrome.runtime.onMessage.addListener(function(mp3_url, sender, sendResponse) {
 
-	console.log(mp3_url);
+	console.log("Downloading: "+mp3_url);
+	/*
 	chrome.downloads.download({
 		url: mp3_url,
 		saveAs: false
 	});
+	*/
 });
 
 //end original code
--- browser-extentions/chrome/c2c-dl/js/content.js
+++ browser-extentions/chrome/c2c-dl/js/content.js
@@ -1,16 +1,10 @@
-
-//var filter = {urls: <all_urls>};
-//chrome.runtime.sendMessage("https://www.coasttocoastam.com/show/2018/09/01");
-
-
 // there is no sleep, so make some
-
 
 function sleep (time) {
   return new Promise((resolve) => setTimeout(resolve, time));
 }
 
-// huge hack
+// base url and date arrays for show path.
 var baseURL = 'https://www.coasttocoastam.com/show/';
 var years = ['2013', '2014', '2015', '2016', '2017', '2018'];
 var months = [ '01', '02', '03', '04', '05', '06', '07', '08', '09', '10',
@@ -44,13 +38,9 @@ for (var y = 0; y < years.length; y++) {
 
 console.log("Current Location: "+window.location.href);
 console.log("Next Location: "+all_dates[all_dates.indexOf(window.location.href)+1]);
-// end huge hack
 
-
-      // sleep until all DOM elements are populated.. else youll get # instead of URLs
-
+// sleep until all DOM elements are populated.. else youll get # instead of URLs
 sleep(500).then(() => {
-  //  var filter = {urls: ["https://www.coasttocoastam.com/*"]};
   // useful links are of the streamlink-dynamic class
   var streamLinks = document.getElementsByClassName("streamlink-dynamic");
   for(var i=0; i<streamLinks.length; i++) {
