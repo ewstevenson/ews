67e756e9e0ea58053296bbf07cfb23ab9ece7c6d
ews <ews@ews.consulting>
2019-02-18 13:47:16 -0700

    hackerrank

---
+++ py/hackerank/loops.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python
+
+if __name__ == '__main__':
+    n = int(raw_input())
+    i = 0
+    if ( n > 0 ):
+        while i < n:
+         print i*i
+         i += 1
+
+
+
+
+#Task
+#Read an integer . For all non-negative integers , print . See the sample for details.
+#
+#Input Format
+#
+#The first and only line contains the integer, .
+#
+#Constraints
+#
+#Output Format
+#
+#Print  lines, one corresponding to each .
+#
+#Sample Input 0
+#
+#5
+#Sample Output 0
+#
+#0
+#1
+#4
+#9
+#16
---
+++ py/hackerank/print_function.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python
+
+from __future__ import print_function
+
+if __name__ == '__main__':
+    n = int(raw_input())
+    for i in xrange(1,n+1): print(i, end='')
+    
+#Read an integer .
+#
+#Without using any string methods, try to print the following:
+#
+#
+#Note that "" represents the values in between.
+#
+#Input Format
+#
+#The first line contains an integer .
+#
+#Output Format
+#
+#Output the answer as explained in the task.
+#
+#Sample Input 0
+#
+#3
+#Sample Output 0
+#
+#123
